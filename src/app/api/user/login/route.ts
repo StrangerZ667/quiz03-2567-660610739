ce755a531584e54bd44c4083979326d85d02ed87
Stranger667 <88589797+StrangerZ667@users.noreply.github.com>
2024-09-26 14:38:03 +0700

    V1

--- src/app/api/user/login/route.ts
+++ src/app/api/user/login/route.ts
@@ -4,17 +4,30 @@ import { DB, readDB } from "@lib/DB";
 import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (request: NextRequest) => {
+  const body = await request.json();
+  const { username, password } = body;
   readDB();
+  const user = DB.users.find(
+    (user:any) => user.username === username && user.password === password
+  );
 
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "Username or Password is incorrect",
-  //   },
-  //   { status: 400 }
-  // );
+  if (!user) {
+    return NextResponse.json(
+     {
+       ok: false,
+       message: "Username or Password is incorrect",
+     },
+     { status: 400 }
+   );
+  }
 
-  const token = "Replace this with token creation";
+  const secret = process.env.JWT_SECRET || "This is my special secret";
+
+  const token = jwt.sign(
+    { username, role: user.role, studentId: user.studentId },
+    secret,
+    { expiresIn: "8h" }
+  );
 
   return NextResponse.json({ ok: true, token });
 };
