d5b938439b805ad0b74d07a893ec9bf18fe58fd1
Stranger667 <88589797+StrangerZ667@users.noreply.github.com>
2024-09-26 15:59:33 +0700

    V5

--- src/app/api/rooms/route.ts
+++ src/app/api/rooms/route.ts
@@ -1,4 +1,4 @@
-import { DB, readDB, writeDB, Database, Payload} from "@lib/DB";
+import { DB, readDB, writeDB, Database} from "@lib/DB";
 import { checkToken } from "@lib/checkToken";
 import { nanoid } from "nanoid";
 import { NextRequest, NextResponse } from "next/server";
